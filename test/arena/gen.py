704924186b5e522a08edfb0dd8ed80bcd12cb31d
Borja García <debuti@gmail.com>
2026-02-08 00:35:03 +0100

    Added new hooks and internal docs

--- test/arena/gen.py
+++ test/arena/gen.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
 
-from pathlib import Path
-import os
-from faker import Faker
 import hashlib
-import sys
 import shutil
+from pathlib import Path
+
+from faker import Faker
 
 SLIDE_DEFAULT_NAME = "Slides"
 volumes = ["Laptop", "Server", "Pendrive"]
+
 
 def del_folder(p):
     try:
         shutil.rmtree(p, ignore_errors=True)
         p.rmdir()
-    except: pass
+    except Exception:
+        pass
+
 
 if __name__ == "__main__":
     script_path = Path(__file__).resolve().parent
-    
+
     Faker.seed(0)
     fake = Faker()
 
@@ -36,35 +38,55 @@ if __name__ == "__main__":
             num_of_folders = fake.random_int(min=1, max=5)
 
             for _ in range(num_of_folders):
-                folder_kind = fake.random_element(elements=('audio', 'video', 'image', 'office', 'text'))
+                folder_kind = fake.random_element(
+                    elements=("audio", "video", "image", "office", "text")
+                )
 
-                folder_path = original_folder.joinpath(volume).joinpath(SLIDE_DEFAULT_NAME).joinpath(slide).joinpath(folder_kind)
+                folder_path = (
+                    original_folder.joinpath(volume)
+                    .joinpath(SLIDE_DEFAULT_NAME)
+                    .joinpath(slide)
+                    .joinpath(folder_kind)
+                )
                 folder_path.mkdir(parents=True, exist_ok=True)
                 print(f"{folder_path} created")
 
-                expected_folder.joinpath(volume).joinpath(SLIDE_DEFAULT_NAME).joinpath(slide).joinpath(folder_kind).mkdir(parents=True, exist_ok=True)
+                (
+                    expected_folder.joinpath(volume)
+                    .joinpath(SLIDE_DEFAULT_NAME)
+                    .joinpath(slide)
+                    .joinpath(folder_kind)
+                ).mkdir(parents=True, exist_ok=True)
 
-                folder_path_ = expected_folder.joinpath(slide).joinpath(SLIDE_DEFAULT_NAME).joinpath(slide).joinpath(folder_kind)
+                folder_path_ = (
+                    expected_folder.joinpath(slide)
+                    .joinpath(SLIDE_DEFAULT_NAME)
+                    .joinpath(slide)
+                    .joinpath(folder_kind)
+                )
                 folder_path_.mkdir(parents=True, exist_ok=True)
 
                 num_of_files = fake.random_int(min=1, max=10)
 
-                for _ in range(num_of_files):  
-                    file_path = folder_path.joinpath(fake.file_name(category=folder_kind))
-                    num_of_bytes = fake.random_int(min=1, max=10)
+                for _ in range(num_of_files):
+                    file_path = folder_path.joinpath(
+                        fake.file_name(category=folder_kind)
+                    )
+                    num_of_bytes = fake.random_int(min=1, max=10) * 1024
 
-                    file_path.write_text(fake.text(max_nb_chars=num_of_bytes*1024))
+                    file_path.write_text(fake.text(max_nb_chars=num_of_bytes))
 
                     sha256_hash = hashlib.sha256()
-                    with open(file_path, "rb") as f, open(f"{file_path}.sha256", "w") as cf:
+                    with open(file_path, "rb") as f, open(
+                        f"{file_path}.sha256", "w"
+                    ) as cf:
                         for byte_block in iter(lambda: f.read(4096), b""):
                             sha256_hash.update(byte_block)
                         checksum = sha256_hash.hexdigest()
                         cf.write(checksum)
-                    
+
                     shutil.copy(file_path, folder_path_)
                     shutil.copy(f"{file_path}.sha256", folder_path_)
 
     shutil.copytree(original_folder, processed_folder)
     shutil.copy(script_path.joinpath("test.conf"), processed_folder)
-                    
